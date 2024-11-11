d7c9784033e3aa7ab0b512d6126b8ce6a5404c2b
ReeceKeys <rmharris333@yahoo.com>
2024-11-10 20:11:51 -0800

    First commit

---
+++ PostCheckApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+        // Command to open the Sound Settings in Windows
+        string soundSettingsCommand = "ms-settings:sound";
+
+        // Start the process to open the Sound settings
+        Process.Start(new ProcessStartInfo
+        {
+            FileName = soundSettingsCommand,
+            UseShellExecute = true
+        });
+
+        Console.WriteLine("Opening Sound Settings...");
+    }
+}
